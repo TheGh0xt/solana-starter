3af3cff5616ec66b842c4f0956309100f07e224d
TheGh0xt <adetoyeanointing15@gmail.com>
2026-02-07 08:33:49 +0100

    chores: create, mint and document NFT token hashs and reflections

--- README.md
+++ README.md
@@ -20,3 +20,25 @@
 - 2VFqkmqGc4WR4VxFvFfw66zeRfCpB9i6LutHtyfEyNmpYDnWn5jeQU42GZWRmsZECXqgoNQYEVhjSUiXpAz5Cqud --- Transferred 100 tokens to wallet
 
 ![transfer-mint-page-on-solscan](images/image-2.png)
+
+## turbin3-nft-mint
+
+- aEXvAQ57sFzssbjFJnWTu6qcAUEXYWoX11NjkW3kBMVNrDLP717Q62HcpAAJjuHMaNY87aa5ixhBqkgqHASkADV -- NFT Mint TX Hash
+
+- https://explorer.solana.com/tx/aEXvAQ57sFzssbjFJnWTu6qcAUEXYWoX11NjkW3kBMVNrDLP717Q62HcpAAJjuHMaNY87aa5ixhBqkgqHASkADV?cluster=devnet -- NFT Mint Page On Solscan
+
+- {transaction hash goes here after trade} -- NFT Transfer TX Hash
+
+![TheGh0xt-NFT](images/nft-screenshot.png)
+
+* Reflection
+- Problem with trading NFT on discord:
+  - It takes longer for people to respond to your trade request.
+  - You have to trust the person you are trading with.
+  - You have to manually send the NFT to the other person.
+
+- Proposed Solution:
+  - Create a trustless NFT marketplace on Solana.
+  - Users can list their NFTs for sale and trade with other users seamlessly and instantly without fear of being scammed due to manual hold and delays.
+  
+
--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -1,9 +1,20 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  generateSigner,
+  percentAmount,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import {
+  createNft,
+  mplTokenMetadata,
+  TokenStandard,
+} from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "/Users/TheGh0xt/.config/solana/id.json";
 import base58 from "bs58";
+// import { create } from "domain";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
@@ -11,16 +22,32 @@ const umi = createUmi(RPC_ENDPOINT);
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
-(async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+const myPhantomWallet = publicKey("xyZPn5pyX8KmSg5uAcJtmPtHprzpo6PhYHQmHQV42W6");
 
-    console.log("Mint Address: ", mint.publicKey);
+const metadataUri =
+  "https://gateway.irys.xyz/FNmCAGrgQERAUbmPFEeUBu4CQEWif3PSS5L2snj1d8fE";
+
+(async () => {
+  let tx = createNft(umi, {
+    mint,
+    name: "The Gh0xt NFT",
+    uri: metadataUri,
+
+    sellerFeeBasisPoints: percentAmount(100, 2),
+    updateAuthority: myKeypairSigner,
+    tokenOwner: myPhantomWallet,
+  });
+
+  let result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
+
+  console.log(
+    `Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`,
+  );
+
+  console.log("Mint Address: ", mint.publicKey);
 })();
